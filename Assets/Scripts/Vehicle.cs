1cc30b6dd46d6482a86c651b3f455220d1f4536b
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2022-10-24 09:30:51 +0200

    decision if

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -7,10 +7,15 @@ public class Vehicle : MonoBehaviour
     //VARIABLES
     [SerializeField] private int wheels = 4; //Saves the number of wheels of the vehicle
     [SerializeField] private string name;
+    [SerializeField] private bool isOn;
 
     private void Start()
     {
-        Debug.Log($"The {name} has {wheels} wheels");
+
+        //Debug.Log($"The {name} has {wheels} wheels");
+        if (isOn) {
+            Debug.Log($"{name} is on");
+        }
     }
    
 }
