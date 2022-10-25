6ba56c20c1d9b4120388985330076d662a519976
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2022-10-25 13:45:42 +0200

    logic operations

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -8,24 +8,39 @@ public class Vehicle : MonoBehaviour
     [SerializeField] private int wheels = 4; //Saves the number of wheels of the vehicle
     [SerializeField] private string name;
     [SerializeField] private bool isOn;
-    [SerializeField] public string sound;
+    public string sound; //Vehicle sound
+    [SerializeField] private float gasoline; //Stores the current value of gasoline
+    [SerializeField] private float lowGas; //Indicates if the gasoline lower than this value
+
 
     private void Start()
     {
-
         //Debug.Log($"The {name} has {wheels} wheels");
+
         if (!isOn)
         {
             Debug.Log($"{name} is off");
         }
-        else if (sound!="")
-        {
-            Debug.Log($"{name} is on and does {sound}");
-        }
         else {
-            Debug.Log($"{name} is running");
-        }
+            if (sound != "")
+            {
+                Debug.Log($"{name} is on and does {sound}");
+            }
+            else {
+                Debug.Log($"{name} is running");
+            }
 
+            if (gasoline < lowGas)
+            {
+                Debug.Log($"{name} is running low of gasoline");
+            }
+            else if (gasoline == 0)
+            {
+                Debug.Log($"The {name} doesn't have gasoline");
+            }
+            else { 
+                Debug.Log($"The {name} has {gasoline} L of gasoline");
+            }
+        }
     }
-   
 }
